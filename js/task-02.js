ce62efd1f99c5b42b959bf8296ec0cbe19b90ea1
Yurii <gapirov21@gmail.com>
2023-08-23 11:06:14 +0300

    hw-js-task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,39 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// for (const ingredient of ingredients) {
+//   const navItemEl = document.createElement("li");
+//   navItemEl.classList.add("item");
+//   navItemEl.textContent = ingredient;
+//   console.log(navItemEl);
+
+//   document.body.prependChild(navItemEl);
+// }
+
+const navList = document.querySelector('#ingredients');
+
+const liArray = ingredients.map((ingredient) => {
+  const navItemEl = document.createElement("li");
+  navItemEl.classList.add("item");
+  navItemEl.textContent = ingredient;
+  
+  return navItemEl;
+  
+});
+
+
+navList.append(...liArray);
+
+console.log(navList);
+
+
+
+// for (const ingredient of ingredients) {
+//   const navItemEl = document.createElement("li");
+//   navItemEl.classList.add("item");
+//   navItemEl.textContent = ingredient;
+//   console.log(navItemEl);
+
+//   document.body.prependChild(navItemEl);
+// }
