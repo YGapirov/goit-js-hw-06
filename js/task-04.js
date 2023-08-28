74535491af39fe8aa6620ca2517358fa915815a5
Yurii <gapirov21@gmail.com>
2023-08-28 17:12:28 +0300

    hw6-tsk1-9

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+// Отримуємо посилання на елементи DOM за їх ідентифікаторами
+const counterElement = document.getElementById("counter");
+const valueElement = document.getElementById("value");
+
+// Ініціалізуємо змінну для лічильника
+let counterValue = 0;
+
+// Оновлюємо інтерфейс новим значенням лічильника
+function updateCounter() {
+  valueElement.textContent = counterValue;
+}
+
+// Додаємо прослуховувачів подій для кнопок
+counterElement.addEventListener("click", (event) => {
+  const action = event.target.dataset.action;
+
+  if (action === "increment") {
+    counterValue++;
+  } else if (action === "decrement") {
+    counterValue--;
+  }
+
+  updateCounter(); // Оновлюємо інтерфейс після зміни значення лічильника
+});
+
+// Ініціалізуємо початкове значення лічильника
+updateCounter();
